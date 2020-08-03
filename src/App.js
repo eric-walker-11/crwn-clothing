92ef60a702be139589be93c5c6be741d1c982921
Eric Walker <Eric.Walker2@nutrien.com>
2020-08-03 00:00:17 -0600

    create home page directory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import HomePage from './HomePage';
+import { HomePage } from 'pages';
 import './App.css';
 
 function App() {
-  return <div>
-    <HomePage />
-  </div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
