460078d79f5993c99b47239a7536d0356f22dfa4
Eric Walker <Eric.Walker2@nutrien.com>
2020-08-03 22:31:32 -0600

    add hover animation to MenuItem

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -3,10 +3,11 @@ import './MenuItem.scss';
 
 export default function MenuItem({ imageUrl, size, title }) {
   return (
-    <div
-      className={`${size} menu-item`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className="content">
         <h1 className="title">{ title.toUpperCase() }</h1>
         <span className="subtitle">Shop Now</span>
