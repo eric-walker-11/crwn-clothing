a404ec8220b1b78bc0748bc0e6ea785ff659d1a8
Eric Walker <Eric.Walker2@nutrien.com>
2020-08-07 14:08:20 -0600

    add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Header } from 'components';
 import { HomePage, ShopPage } from 'pages';
 import './App.css';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as CollectionItem } from './CollectionItem';
 export { default as CollectionPreview } from './CollectionPreview';
 export { default as Directory } from './Directory';
+export { default as Header } from './Header';
 export { default as MenuItem } from './MenuItem';
