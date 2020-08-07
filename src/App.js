4862abcc8c356b6754f20140d559316e9ef6118a
Eric Walker <Eric.Walker2@nutrien.com>
2020-08-07 13:32:36 -0600

    add shop page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { HomePage } from 'pages';
+import { HomePage, ShopPage } from 'pages';
 import './App.css';
-
-function HatsPage() {
-  return <h1>HATS PAGE</h1>
-}
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
