ae46d9f3df2cb682d3d2c4d9c61599407e04737a
Eric Walker <Eric.Walker2@nutrien.com>
2020-08-11 08:19:04 -0600

    add login link and logout button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { auth } from 'api/firebase';
 import { Header } from 'components';
 import { HomePage, ShopPage, SignInPage } from 'pages';
 import './App.css';
 
-export default function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signIn' component={SignInPage} />
-      </Switch>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { currentUser: null };
+  }
+
+  unsubscribeFromAuth = () => {};
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(
+      user => this.setState({ currentUser: user })
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/shop' component={ShopPage}/>
+          <Route exact path='/signIn' component={SignInPage}/>
+        </Switch>
+      </div>
+    );
+  }
+};
--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from 'api/firebase';
 import { ReactComponent as Logo } from 'assets/crown.svg';
 import './Header.styles.scss';
 
-export default function Header() {
+export default function Header({ currentUser }) {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -12,6 +13,15 @@ export default function Header() {
       <div className="options">
         <Link className="option" to="/shop">SHOP</Link>
         <Link className="option" to="/contact">CONTACT</Link>
+        {
+          currentUser
+            ? (
+              <div className="option" onClick={() => auth.signOut()}>
+                SIGN OUT
+              </div>
+            )
+            : <Link className="option" to="/signIn">SIGN IN</Link>
+        }
       </div>
     </div>
   );
