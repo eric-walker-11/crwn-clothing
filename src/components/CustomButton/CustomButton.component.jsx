ee03c6d72f5b689a6afb420f3d1af03b56bd6e14
Eric Walker <Eric.Walker2@nutrien.com>
2020-08-12 14:31:51 -0600

    fix required field request on google sign in

--- src/components/CustomButton/CustomButton.component.jsx
+++ src/components/CustomButton/CustomButton.component.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './CustomButton.styles.scss'
 
-export default function CustomButton({ children, isGoogleSignIn, ...otherProps }) {
+export default function CustomButton({
+  children,
+  isGoogleSignIn,
+  type = 'button',
+  ...otherProps
+}) {
   return (
     <button
       className={`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button`}
+      type={type}
       {...otherProps}
     >
       {children}
