d973420518c9284f50b0fe681136325990a0ece8
Eric Walker <Eric.Walker2@nutrien.com>
2020-08-12 14:23:10 -0600

    enable email registration

--- src/pages/SignInPage/SignInPage.component.jsx
+++ src/pages/SignInPage/SignInPage.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { SignIn } from 'components';
+import { SignIn, SignUp } from 'components';
 import './SignInPage.styles.scss';
 
 export default function SignInPage() {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
