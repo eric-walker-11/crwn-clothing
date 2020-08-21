c704991dc37e57e2a5cf54f750b827cb4d2665b9
Eric Walker <Eric.Walker2@nutrien.com>
2020-08-21 11:52:49 -0600

    update quantity instead of duplicating cart items

--- src/framework/redux/cart/cart.reducer.js
+++ src/framework/redux/cart/cart.reducer.js
@@ -1,16 +1,44 @@
 import { ADD_CART_ITEM, TOGGLE_CART_HIDDEN } from './cart.types';
 
 const INITIAL_STATE = {
-  cartItems: [],
+  cartItems: { ids: [] },
   hidden: true
 };
+
+function addCartItem(cartItems, item) {
+  const existingItem = cartItems[item.id];
+
+  if (existingItem) {
+    return {
+      ...cartItems,
+      [item.id]: {
+        ...existingItem,
+        quantity: existingItem.quantity + 1
+      }
+    };
+  }
+
+  return {
+    ...cartItems,
+    ids: [...cartItems.ids, item.id].sort(),
+    [item.id]: { item, quantity: 1 }
+  };
+}
 
 export default function cartReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case ADD_CART_ITEM:
-      return {...state, cartItems: [...state.cartItems, action.payload]};
+      return {
+        ...state,
+        cartItems: addCartItem(state.cartItems, action.payload)
+      };
+
     case TOGGLE_CART_HIDDEN:
-      return { ...state, hidden: !state.hidden };
+      return {
+        ...state,
+        hidden: !state.hidden
+      };
+
     default:
       return state;
   }
