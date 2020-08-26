1dc28639f02a9e121e55e8a36a700e49cf13fa49
Eric Walker <Eric.Walker2@nutrien.com>
2020-08-26 09:39:20 -0600

    add checkout page link to cart dropdown

--- src/framework/react/components/CartDropdown/CartDropdown.component.jsx
+++ src/framework/react/components/CartDropdown/CartDropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 
 import { CartItem, CustomButton } from 'framework/react/components';
@@ -7,7 +8,7 @@ import { selectors } from 'framework/redux';
 
 import './CartCropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {
@@ -18,7 +19,9 @@ const CartDropdown = ({ cartItems }) => (
           : <span className="empty-message">Your cart is empty</span>
       }
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => history.push('/checkout')}>
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
@@ -26,4 +29,4 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectors.selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
